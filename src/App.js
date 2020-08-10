27b494f0a5f97de5aec8b1c299ab399a1500c1d1
フォウニーphonie <wiroon2543@gmail.com>
2020-08-10 10:07:27 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import WordCard from './WordCard';
+
+const word = "Hello";
 
 function App() {
   return (
-    <div className="App">
-     Hello world
+    <div>
+      <WordCard value = {word}/>  
     </div>
   );
 }
-
 export default App;
