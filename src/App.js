7e4b14b4101f91095ea1ebe252e87b3f87217899
フォウニーphonie <wiroon2543@gmail.com>
2020-08-12 11:43:37 +0700

    part 6

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
-
 function App() {
   return (
-    <div>
-      <WordCard value = {word}/>  
-    </div>
+   <div>
+     <WordCard value={word}/>
+   </div>
   );
-}
-export default App;
+ }
+ 
+ export default App;
